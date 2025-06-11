65ca6e527b3c942781bd19b44ef776e032ffb932
jbr284 <jbrosa2009@gmail.com>
2025-06-10 22:14:30 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,21 @@
 
-self.addEventListener('install', function(e) {
-    e.waitUntil(
-        caches.open('agenda-pwa').then(function(cache) {
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('pwa-cache').then((cache) => {
             return cache.addAll([
-                '/',
                 'index.html',
                 'manifest.json',
-                'service-worker.js',
-                'https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js',
-                'https://www.gstatic.com/firebasejs/8.10.0/firebase-database.js'
+                'icon-192x192.png',
+                'icon-512x512.png'
             ]);
         })
     );
 });
 
-self.addEventListener('fetch', function(e) {
-    e.respondWith(
-        caches.match(e.request).then(function(response) {
-            return response || fetch(e.request);
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            return cachedResponse || fetch(event.request);
         })
     );
 });
